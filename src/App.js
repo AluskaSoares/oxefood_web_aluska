18271b57059f7b14a5a4f700d74b60a1e0c2f99d
AluskaSoares <121875450+AluskaSoares@users.noreply.github.com>
2023-11-11 23:04:43 -0300

    Atual.

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Segment } from 'semantic-ui-react';
+// import logo from './logo.svg';
+import './App.css';
+/*import Home from './views/home/Home';
+import FormCliente from './views/cliente/FormCliente';
+import FormProduto from './views/produto/FormProduto';*/
+import Rotas from './Rotas';
+
+
+function App() {
+  return (
+    <div className="App">
+
+<Rotas />
+
+      
+      
+
+      <div style={{marginTop: '6%'}}>
+        <Segment vertical color='grey' size='tiny' textAlign='center'>
+          &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
+        </Segment>
+      </div>
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/views/produto/FormProduto.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import InputMask from 'react-input-mask';
+import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+
+export default function FormProduto () {
+
+    return (
+
+        <div>
+
+            <div style={{marginTop: '3%'}}>
+
+                <Container textAlign='justified' >
+
+                    <h2> <span style={{color: 'darkgray'}}> Produto &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+
+                    <Divider />
+
+                    <div style={{marginTop: '4%'}}>
+
+                        <Form>
+
+                            <Form.Group widths='equal'>
+
+                                <Form.Input
+                                    required
+                                    fluid
+                                    label='Titulo'
+                                    maxLength="100"
+                                />
+
+                                <Form.Input
+                                    required
+                                    fluid
+                                    label='Codigo do produto'>
+                                    <InputMask
+                                        required
+                                        mask="999.999.999-99"
+                                    /> 
+                                </Form.Input>
+                                
+                            </Form.Group>
+                            <Form.TextArea label='Descrição' placeholder='Tell us more about you...' />
+                            <Form.Group>
+
+                                <Form.Input
+                                    fluid
+                                    label='Valor Unitario*'
+                                    width={6}>
+                                    <InputMask 
+                                        mask="(99) 9999.9999"
+                                    /> 
+                                </Form.Input>
+
+                                <Form.Input
+                                    fluid
+                                    label='Tempo de Entrega mínimo em minutos'
+                                    width={6}>
+                                    <InputMask 
+                                        mask="(99) 9999.9999"
+                                    /> 
+                                </Form.Input>
+
+                                <Form.Input
+                                    fluid
+                                    label='Tempo de entrega màximo em minutos'
+                                    width={6}
+                                >
+                                    <InputMask 
+                                        mask="99/99/9999" 
+                                        maskChar={null}
+                                        placeholder="Ex: 20/03/1985"
+                                    /> 
+                                </Form.Input>
+
+                            </Form.Group>
+                        
+                        </Form>
+                        
+                        <div style={{marginTop: '4%'}}>
+
+                            <Button
+                                type="button"
+                                inverted
+                                circular
+                                icon
+                                labelPosition='left'
+                                color='orange'
+                            >
+                                <Icon name='reply' />
+                                Listar
+                            </Button>
+                                
+                            <Button
+                                inverted
+                                circular
+                                icon
+                                labelPosition='left'
+                                color='blue'
+                                floated='right'
+                            >
+                                <Icon name='save' />
+                                Salvar
+                            </Button>
+
+                        </div>
+
+                    </div>
+                    
+                </Container>
+            </div>
+        </div>
+
+    );
+
+}
